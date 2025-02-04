b1ca094ed6967dbfc90cfe4bc6bfd6b57d108c17
Zakaria El Moudni <158088515+ZakariaElM-HELB@users.noreply.github.com>
2025-02-04 22:34:51 +0100

    Base du Projet MAUI

---
+++ MyApp-main/View/MainView.xaml.cs
@@ -0,0 +1,12 @@
+namespace MyApp.View;
+
+public partial class MainView : ContentPage
+{
+	MainViewModel viewModel;
+	public MainView(MainViewModel viewModel)
+	{
+		this.viewModel = viewModel;
+		InitializeComponent();
+		BindingContext=viewModel;
+	}
+}
---
+++ MyApp-main/ViewModel/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.ViewModel;
+
+public partial class MainViewModel: ObservableObject
+{
+    [ObservableProperty]
+    private string myVar = "Blabla";
+
+    [RelayCommand]
+    internal void ChangeBindedLabel()
+    {
+        MyVar += "blabla";
+    }
+}
